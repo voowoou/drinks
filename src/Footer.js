a60adcdc6a7360940a174907526936d15009aa1d
Nikolai Bik <voowoou@gmail.com>
2023-11-24 20:43:41 +0300

    delete dev files

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./Footer.module.css";
-
-export default function Footer() {
-    return (
-        <p className={styles.footer}>
-            this educational app uses API by CoctailDB
-        </p>);
-}
